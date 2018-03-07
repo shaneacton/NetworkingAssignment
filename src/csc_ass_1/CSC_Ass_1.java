bb52d450db30f56b1a83c79544dd595cf4b73f35
Liron Toledo <tldlir001@myuct.ac.za>
2018-03-07 12:18:56 +0200

    first commit

---
+++ src/csc_ass_1/CSC_Ass_1.java
@@ -0,0 +1,122 @@
+
+package csc_ass_1;
+
+import java.io.*;
+import java.net.*;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+
+public class CSC_Ass_1 
+{
+
+    
+    public static void main(String[] args)
+    {
+        Socket MyClient = null;
+        
+        try 
+        {
+            MyClient = new Socket("Chatbot",1050);
+        } 
+        catch (IOException ex) 
+        {
+            System.out.println(ex);
+        }
+        
+        
+        ServerSocket MyService = null;
+        
+        try 
+        {
+            MyService = new ServerSocket(1050);
+        } 
+        catch (IOException ex) 
+        {
+            System.out.println(ex);
+        }
+        
+        Socket serviceSocket = null;
+        
+        try
+        {
+            serviceSocket = MyService.accept();
+        }
+        catch(IOException ex)
+        {
+            System.out.println(ex);
+        }
+        
+        
+        
+        DataInputStream input = null;
+        
+        try
+        {
+            input = new DataInputStream (MyClient.getInputStream());
+        }
+        catch(IOException ex)
+        {
+            System.out.println(ex);
+        }
+        
+        DataInputStream input2 = null;
+        
+        try
+        {
+            input2 = new DataInputStream(serviceSocket.getInputStream());
+        }
+        catch(IOException ex)
+        {
+            System.out.println(ex);
+        }
+        
+        DataOutputStream output = null;
+        
+        try
+        {
+            output = new DataOutputStream(MyClient.getOutputStream());
+        }
+        catch(IOException ex)
+        {
+            System.out.println(ex);
+        }
+        
+        PrintStream output2 = null;
+        
+        try
+        {
+            output2 = new PrintStream(serviceSocket.getOutputStream());
+        }
+        catch(IOException ex)
+        {
+            System.out.println(ex);
+        }
+            
+        try
+        {
+            output.close();
+            input.close();
+            MyClient.close();
+        }
+        catch(IOException ex)
+        {
+            System.out.println(ex);
+        }
+        
+        try
+        {
+            output2.close();
+            input2.close();
+            serviceSocket.close();
+            MyService.close();
+        }
+        catch(IOException ex)
+        {
+            System.out.println(ex);
+        }
+        
+        
+    }
+    
+}
